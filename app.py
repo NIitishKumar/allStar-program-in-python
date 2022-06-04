b0e5a4cf07a0c7f1c5f883fcfc93e6e73579cf89
NIitishKumar <51985984+NIitishKumar@users.noreply.github.com>
2022-06-05 01:55:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,63 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+
+n = 5
+for i in range(1,n+1):
+    for j in range(1,2):
+        print(' '*(n-i)  + '* '*i)
+        
+        
+
+for i in range(1, n + 1):
+    print('*'*i)
+    
+
+for i in range(1, n + 1):
+    for j in range(1,2):
+        print(' '*(n-i) + '*'*i)
+        
+        
+for i in range(1,n+1):
+    print('*'*i)
+    if i == n:
+        for j in range(1,n):
+            print('*'*(n-j))
+            
+            
+for i in range(0,n):
+    print('*'*(n-i))
+    
+for i in range(0,n+1):
+    print( ' '*i+ '* '*(n-i))
+    
+for i in range(1,n+1):
+    print(' '* (n-i) + '* ' * i)
+    if (i == n):
+        for j in range(1,n+1):
+            print(' '*j + '* '*(n-j))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
